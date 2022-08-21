2c3cf575e1aa36cb08d52d669c949826366411b1
Atharva <atharvapradeeprao.k20@iiits.in>
2022-08-21 23:00:57 +0530

    uid: db updated

--- models/aicte.py
+++ models/aicte.py
@@ -3,12 +3,12 @@ from db import db
 class AicteModel(db.Model):
     __tablename__ = 'aicte'
 
-    email = db.Column(db.String(80))
+    email = db.Column(db.String(80),primary_key=True)
     password = db.Column(db.String(150))
     phone = db.Column(db.String(15))
     gender = db.Column(db.String(20))
     user_type = db.Column(db.String(20))
-    aadhar = db.Column(db.String(14),primary_key=True)
+    aadhar = db.Column(db.String(14))
     aadhar_remark = db.Column(db.String(50), default='Upload Aadhar to verify')## Aadhar remark
     aadhar_date = db.Column(db.DateTime(timezone=True), default=None)
     seeded_bank_acc = db.Column(db.String(30))
@@ -23,13 +23,26 @@ class AicteModel(db.Model):
     dob = db.Column(db.DateTime(timezone=True), default=None)
     admission_year=db.Column(db.String(5))
 
-    def __init__(self, aadhar, seeded_bank_acc,pan,name,college,gender):
+    def __init__(self,email,password,phone,gender,user_type,aadhar,aadhar_remark,aadhar_date, seeded_bank_acc,seeded_remark,seeded_date,pan,pan_remark,pan_date,name,college,address,dob,admission_year):
+        self.email=email
+        self.password=password
+        self.phone=phone
+        self.gender = gender
+        self.user_type=user_type
         self.aadhar = aadhar
+        self.aadhar_remark=aadhar_remark
+        self.aadhar_date=aadhar_date
         self.seeded_bank_acc = seeded_bank_acc
         self.pan = pan
         self.name = name
         self.college = college
-        self.gender = gender
+        self.seeded_remark=seeded_remark
+        self.seeded_date=seeded_date
+        self.pan_remark=pan_remark
+        self.pan_date=pan_date
+        self.address=address
+        self.dob=dob
+        self.admission_year=admission_year
 
     def save_to_db(self):
         db.session.add(self)
@@ -42,3 +55,7 @@ class AicteModel(db.Model):
     @classmethod
     def find_by_pan(cls, pan):
         return cls.query.filter_by(pan=pan).first()
+
+    @classmethod
+    def find_by_email(cls,email):
+        return cls.query.filter_by(email=email).first()
