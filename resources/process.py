35cd224153ad762382ae7a0d815e886990cdffab
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-08-22 23:44:09 +0530

    Pan: Text Extraction Integrated

--- resources/process.py
+++ resources/process.py
@@ -5,9 +5,11 @@ from models.aicte import AicteModel
 from models.uid import UidModel
 from models.npci import NpciModel
 from models.aicte import AicteModel
+from models.pan import PanModel
 from util.response import HttpApiResponse, HttpErrorResponse
 from util.time import nowTime
 import requests,os
+from subprocess import call
 
 class ProcessAadhar(Resource):
     def post(self):
@@ -85,7 +87,6 @@ class ProcessAadhar(Resource):
             user.aadhar = aadharNumber
             user.aadhar_remark = 'Aadhar Verified Successfully'
             user.address = uidData.address
-            user.dob = uidData.dob
             user.last_updated = nowTime()
             user.save_to_db()
             print('[Process:ProcessAadhar] Verified & updated aadhar details from UID Database | AadharNo='+ aadharNumber)
@@ -119,3 +120,78 @@ class ProcessAadhar(Resource):
 
         ## Send the final result if UID and NPCI is verified or not
         return HttpApiResponse({'aadharVerified': uidVerified, 'npciVerified': npciVerified}), 200
+
+
+
+## Process of PAN after Upload
+class ProcessPan(Resource):
+    def post(self):
+        
+        ## Get the data from [Upload:UploadPan] 
+        input_json = request.get_json(force=True) 
+        user_email = input_json['user_email']
+        name = input_json['name']
+
+        ## Get user and set remarks of pan to verifying for frontend
+        user = AicteModel.find_by_email(user_email)
+        if not user:
+            return HttpErrorResponse ("No user found with this email"), 404
+        
+        ## Check if aadhar already present then don't go further
+        if user.pan:
+            return HttpErrorResponse ("Cannot upload, not a new user"), 400
+
+        user.pan_remark = 'Verifying Uploaded Data'
+        user.save_to_db()
+
+        ## Send it to the ML model to extract the card details
+        call(["python3","ml/Pan/PanDict.py"])
+        f=open("write.txt","r")
+        panNumber = f.read()
+        print('[Process:ProcessPan] Pan model execution done | User='+ user_email + ' | PanNo='+ panNumber)
+        os.remove('images/'+name)
+
+        postDict={'email':user_email,'msg':""}
+
+        ## If pan NA then upload again
+        if panNumber == 'NA' or panNumber == ' ' or panNumber == '':
+            print('[Process:ProcessPan] Upload again, pan image not clear | User='+ user_email + ' | --ExitProcess--')
+            user.pan_remark = 'Upload pan again, image not clear'
+            user.save_to_db()
+
+            #sending mail
+            postDict['msg']='Dear '+user.name+', your Pan could not be verified because it was unclear. Please re-upload a clear photo of your Pan on the AICTE portal.'
+            requests.post(os.getenv("EMAIL_URL"),json=postDict)
+
+            return HttpErrorResponse ("Upload again, image not clear"), 400
+
+
+        ## Check it with PAN database
+        panData = PanModel.find_by_pan(panNumber)
+        if not panData:
+            print('[Process:ProcessPan] Pan does not exists in PAN database | PanNo='+ panNumber)
+            user.pan_remark = 'Failed, PanNo does not exist with PAN'
+            user.save_to_db()
+
+            #sending mail
+            postDict['msg']='Dear '+user.name+', the data corresponding to details uploaded by you could not be verified from PAN database. Please re-upload the document or contact college administration for more details.'
+            requests.post(os.getenv("EMAIL_URL"),json=postDict)
+            
+            return HttpErrorResponse ('Pan does not exists in PAN database'), 404
+
+        else:
+            user.pan = panNumber
+            user.pan_remark = 'Pan Verified Successfully'
+            user.dob = panData.dob
+            user.last_updated = nowTime()
+            user.save_to_db()
+
+            print('[Process:ProcessPan] Verified & updated pan details from PAN Database | PanNo='+ panNumber)
+
+            #sending email
+            postDict['msg']='Dear '+user.name+', your details were successfully verified!. Please login to the AICTE portal for more details.'
+            print(postDict['email'],postDict['msg'])
+            requests.post(os.getenv("EMAIL_URL"),json=postDict)
+
+        ## Send the final result if PAN is verified or not
+        return HttpApiResponse({'panVerified': True}), 200
