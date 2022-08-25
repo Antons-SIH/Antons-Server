3494affa92b070a374c0bfddfb3df7d6e4d54c42
Atharva <atharvapradeeprao.k20@iiits.in>
2022-08-26 00:24:31 +0530

    aadhar: upload

--- resources/upload.py
+++ resources/upload.py
@@ -25,7 +25,7 @@ class UploadAadhar(Resource):
         img_file = open('aadhar.jpeg', 'wb')
         img_file.write(decoded_data)
         img_file.close()
-        name="aadhar.jpeg"
+        name="aadhar"
         # print(image_file)
         ## Check if user exist with this email
         user = AicteModel.find_by_email(user_email)
