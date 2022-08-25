e794d73f49d5a9b5e90e9e09e7734e4152efd1a2
Atharva <atharvapradeeprao.k20@iiits.in>
2022-08-26 00:22:39 +0530

    aadhar: upload

--- resources/process.py
+++ resources/process.py
@@ -33,7 +33,8 @@ class ProcessAadhar(Resource):
         user.save_to_db()
 
         ## Send it to the ML model to extract the card details
-        Dict = getAadharDictionary('images/'+name)
+        # Dict = getAadharDictionary('images/'+name)
+        Dict = getAadharDictionary(name)
         aadharNumber = Dict["aadharNumber"]
         print('[Process:ProcessAadhar] Aadhar model execution done | User='+ user_email + ' | AadharNo='+ aadharNumber)
         os.remove('images/'+name)
--- resources/upload.py
+++ resources/upload.py
@@ -22,10 +22,10 @@ class UploadAadhar(Resource):
         filestring = request.form['filestring']
 
         decoded_data=base64.b64decode((filestring))
-        img_file = open('images/image.jpeg', 'wb')
+        img_file = open('aadhar.jpeg', 'wb')
         img_file.write(decoded_data)
         img_file.close()
-        name="image.jpeg"
+        name="aadhar.jpeg"
         # print(image_file)
         ## Check if user exist with this email
         user = AicteModel.find_by_email(user_email)
